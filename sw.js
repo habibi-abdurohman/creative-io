df1be272cff94f7670bcd29d595bf86d64b5c5ec
habibi-abdurohman <thelife.ofhabibi@gmail.com>
2026-05-12 08:47:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
-const CACHE_NAME = 'creative-cache-v1';
+const CACHE_NAME = 'creative-v1';
 
 const urlsToCache = [
-
   './',
   './index.html',
-
-  './notepad.html',
-  './calculator.html',
-  './planner.html',
-
-  './manifest.json',
-
-  './navbar/navbar.css',
-  './navbar/navbar.js',
-  './navbar/navbar.html'
+  './manifest.json'
 ];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+
+  );
+
+});
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+
+  );
+
+});
