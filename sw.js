35b67c35bb7d0d175ba2e131c7cffc22bcd57a6b
habibi-abdurohman <thelife.ofhabibi@gmail.com>
2026-05-12 08:39:08 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'creative-cache-v1';
+
+const urlsToCache = [
+
+  './',
+  './index.html',
+
+  './notepad.html',
+  './calculator.html',
+  './planner.html',
+
+  './manifest.json',
+
+  './navbar/navbar.css',
+  './navbar/navbar.js',
+  './navbar/navbar.html'
+];
