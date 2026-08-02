c9c6c159ff2e150e67fb049a8f830c38749a9015
habibi-abdurohman <thelife.ofhabibi@gmail.com>
2026-08-02 17:47:01 +0700

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,15 +1,11 @@
 import { auth } from './firebase.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 const isInsidePagesFolder = window.location.pathname.includes('/pages/') || window.location.pathname.includes('/collab/');
 const rootPath = isInsidePagesFolder ? '../' : './';
-
 const publicPages = ['index.html', 'login.html', 'register.html', 'forgot-password.html', ''];
 const currentPage = window.location.pathname.split('/').pop() || 'index.html';
 const isPublicPage = publicPages.includes(currentPage);
-
 const USE_FIREBASE = true;
-
 if (USE_FIREBASE) {
     onAuthStateChanged(auth, (user) => {
         if (user) {
